919991fb1362a11efb8ac022fdf7d1c7998d64d8
Anirudha Bele <beleanirudha8@gmail.com>
2025-10-21 18:42:59 +0530

    feat(01-error-handling-in-express/express-error-handler-api/app.js): import and use error handling middlewares in app.js

--- 01-error-handling-in-express/express-error-handler-api/app.js
+++ 01-error-handling-in-express/express-error-handler-api/app.js
@@ -1,11 +1,25 @@
 import express from "express";
+import globalErrorhandler, {
+	handleNotFound,
+} from "./middleware/globalErrorHandler";
 
 const app = express();
 
 app.use(express.json());
 
+// All routes goes here...
 app.get("/", (req, res) => {
 	res.send("Express Error Handling Project Initialized ✅");
 });
+
+/**
+ * More routes will be come here, in future...
+ **/
+
+// For unhandled routes
+app.all("*", handleNotFound);
+
+// Global error middleware
+app.use(globalErrorhandler);
 
 export default app;
