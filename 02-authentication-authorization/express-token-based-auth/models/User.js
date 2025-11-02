0a4516b08c99396a213b150262fa040d455a73fb
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-02 15:31:33 +0530

    feat(02-authentication-authorization/express-token-based-auth/models/User.js): create method to compare entered password with stored hashed password

--- 02-authentication-authorization/express-token-based-auth/models/User.js
+++ 02-authentication-authorization/express-token-based-auth/models/User.js
@@ -30,13 +30,18 @@ const userSchema = new mongoose.Schema(
 );
 
 // encrypt password before saving
-userSchema.pre("save", async (next) => {
+userSchema.pre("save", async function (next) {
 	if (!this.isModified("password")) return next();
 
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 	next();
 });
+
+// compare entered password with stored hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
