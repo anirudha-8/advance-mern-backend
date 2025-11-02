56c0999f8068552897da74b98e1a04e88f9659f2
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-02 15:25:25 +0530

    feat(02-authentication-authorization/express-token-based-auth/models/User.js): create user model schema

---
+++ 02-authentication-authorization/express-token-based-auth/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please, enter your name!"],
+		},
+		email: {
+			type: String,
+			required: [true, "Please, enter your email!"],
+			unique: true,
+			lowercase: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Please, enter your password!"],
+			minlength: 6,
+		},
+		role: {
+			type: String,
+			enum: ["user", "admin"],
+			default: "user",
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
