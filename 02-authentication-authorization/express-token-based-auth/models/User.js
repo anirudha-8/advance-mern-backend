f382aca1327119cfa8ed7e380461b0ae023d1b58
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-02 15:29:21 +0530

    feat(02-authentication-authorization/express-token-based-auth/models/User.js): encrypt password before saving

--- 02-authentication-authorization/express-token-based-auth/models/User.js
+++ 02-authentication-authorization/express-token-based-auth/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
 	{
@@ -27,6 +28,15 @@ const userSchema = new mongoose.Schema(
 		timestamps: true,
 	}
 );
+
+// encrypt password before saving
+userSchema.pre("save", async (next) => {
+	if (!this.isModified("password")) return next();
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
 
 const User = mongoose.model("User", userSchema);
 
