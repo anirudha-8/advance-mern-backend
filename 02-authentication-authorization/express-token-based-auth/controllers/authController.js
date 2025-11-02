5b5bc0878b15e2f55e1cdb5a921cdbd3b37689ab
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-02 15:57:32 +0530

    feat(02-authentication-authorization/express-token-based-auth/controllers/authController.js): create new user register controller

---
+++ 02-authentication-authorization/express-token-based-auth/controllers/authController.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+
+/**
+ * Register Controller
+ * @desc 	Register new user
+ * @route 	GET /auth/register
+ * @access 	Public
+ */
+export const registerUser = async (req, res) => {
+	try {
+		const { name, email, password } = req.body;
+
+		// validate input
+		if (!name || !email || !password) {
+			return res.status(400).json({ message: "All fields are required!" });
+		}
+
+		// check if user is already exists
+		const existingUser = await User.findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ message: "User already exists!" });
+		}
+
+		// create new user
+		const newUser = await User.create({ name, email, password });
+
+		// send success response
+		res.status(201).json({
+			_id: newUser._id,
+			name: newUser.name,
+			email: newUser.email,
+			role: newUser.role,
+		});
+	} catch (error) {
+		console.error("Error in registration of user: ", error);
+		res.status(500).json({ message: "Internal Server Error!" });
+	}
+};
