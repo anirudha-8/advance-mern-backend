1a7ab8a29e2e3ae81df21eba95650564517b3655
Anirudha Bele <beleanirudha8@gmail.com>
2025-11-04 22:43:40 +0530

    feat(02-authentication-authorization/express-token-based-auth/routes/authRoutes.js): implement user login route

--- 02-authentication-authorization/express-token-based-auth/routes/authRoutes.js
+++ 02-authentication-authorization/express-token-based-auth/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // @desc - register route
 router.post("/register", registerUser);
+
+// @desc - login route
+router.post("/logic", loginUser);
 
 export default router;
